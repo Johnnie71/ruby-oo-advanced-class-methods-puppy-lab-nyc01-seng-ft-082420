710ab761ff01d4dcee9f3603ae5a4339e03454e9
johnnie71 <johnnie.agonz71@gmail.com>
2020-08-30 18:11:44 -0400

    Added methods and notes

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+
+
+
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+
+    def initialize(name)
+        @name = name
+        save   #saves the data automatically when created
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+
+    def self.print_all
+        puts @@all.map { |dog| dog.name }   # creates an array of all Dog names
+    end
+
+    def save
+        @@all << self  # automatically pushed newly created object in the constant array
+    end
+
+end
